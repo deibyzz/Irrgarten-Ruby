ef89e42acfb16e858206cd9721503c088f0865e6
deibyzz <davileti.mg@gmail.com>
2025-03-23 13:11:58 +0100

    Empezada práctica 2, necesario preguntar implementación de las matrices del laberinto y los atributos ROW COL

--- TestP1.rb
+++ TestP1.rb
@@ -1,19 +1,9 @@
 #encoding:utf-8
-require_relative 'Weapon'
+require_relative 'Monster'
 require_relative 'Dice'
 require_relative 'Shield'
 
 module Irrgarten
-  arma = Weapon.new(Dice.weapon_power,Dice.uses_left)
-  discard = false
-  contador = 0
-  for i in (0..99)
-    discard = arma.discard
-    puts(discard)
-    if(discard)
-      contador +=1
-    end
-  end
-  puts(arma.to_s)
-  puts(contador)
+  monstruo = Monster.new("Dinga",Dice.random_intelligence(),Dice.random_strength())
+  puts(monstruo.to_s)
 end
